e5ef90f5ea1a48a940a0b757e78b01e63e9e1bf5
M7FAWZY <116055747+M7FAWZY@users.noreply.github.com>
2023-03-18 15:23:47 +0200

    Update subdag.py

--- home/airflow/dags/subdag.py
+++ home/airflow/dags/subdag.py
@@ -6,19 +6,22 @@ from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 
 from airflow.operators.subdag_operator import SubDagOperator
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
 from helpers import SqlQueries
-
+import sql
 #subDAG for load_dimension_table
-def load_dimension__table(
+def load_dimension_subdag(
         parent_dag_name,
         task_id,
         redshift_conn_id,
         aws_credentials_id,
         table,
-        create_sql_stmt,
-        insert_sql_stmt,
+        create_query,
+        insert_query,
         s3_bucket,
         s3_key,
+        delete_load,        
         *args, **kwargs):
     dag = DAG(
         f"{parent_dag_name}.{task_id}",
@@ -29,15 +32,15 @@ def load_dimension__table(
     create_task = PostgresOperator(
         task_id=f"create_{table}_table",
         dag=dag,
-        postgres_conn_id=conn_id,
-        sql=create_sql_stmt
+        postgres_conn_id=redshift_conn_id,
+        sql=create_query
     )
 
 
     load_dimension_table_task = LoadDimensionOperator(
         task_id=f"load_{table}_dimension_table",
         dag=dag,
-        target_table=target_table,
+        table=table,
         redshift_conn_id=redshift_conn_id,
         aws_credentials_id=aws_credentials_id,
         s3_bucket=s3_bucket,
@@ -47,10 +50,10 @@ def load_dimension__table(
     insert_task = PostgresOperator(
         task_id=f"insert_{table}_table",
         dag=dag,
-        postgres_conn_id=conn_id,
-        sql=insert_sql_stmt
+        postgres_conn_id=redshift_conn_id,
+        sql=insert_query
     )    
 
-    #subDAg dependencies
+#subDAg dependencies
     create_task >> load_dimension_table_task >> insert_task
     return dag
